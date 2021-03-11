7a1277f9b0223644a275b746fe9e7d2e69b2d070
Adeyemi Adekanye <sundayadeyemi8@gmail.com>
2021-03-11 10:44:41 +0100

    git

--- mainassignment.js
+++ mainassignment.js
@@ -9,20 +9,23 @@ Your function should return the value of the first argument divided by the secon
 If the value of the third argument is ‘sum’
 Your function should return the sum of the first two arguments */
 
+
 function accessHut(number1, number2, handler){
-    let result;
-      if(handler == "multiply"){
-        result = number1 * number2
-        return result
-    } else if (handler == "divide"){
-        result = number1 / number2
-        return result;
-   } else if(handler=="sum"){
-      result = number1 + number2;
+  let result;
+    if(handler == "multiply"){
+      result = number1 * number2
+      return result
+  } else if (handler == "divide"){
+      result = number1 / number2
       return result;
-  }
-  }
-  
-  console.log(accessHut(3,5,"sum"));
-  console.log(accessHut(3,5,"multiply"));
-  console.log(accessHut(3,5,"divide"));
+ } else if(handler=="sum"){
+    result = number1 + number2;
+    return result;
+}else{
+  console.log('Please enter the correct input');
+}
+}
+
+console.log(accessHut(3,5,"sum"));
+console.log(accessHut(3,5,"multiply"));
+console.log(accessHut(3,5,"divide"));
